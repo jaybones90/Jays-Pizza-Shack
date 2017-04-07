25f6ff6edd296bd72ffd47bacc968a714e4a2f43
Jason Ainsworth <jasoncruze@gmail.com>
2017-04-07 12:01:55 -0700

    add in display div to show user their selected options

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,61 @@
 function Pizza(size, toppings, cost) {
   this.size = size;
-  this.toppings = [];
-  this.cost = cost;
+  this.toppings = 0;
+  this.cost = 0;
 }
 
-Pizza.prototype.getTotalCost = function() {
-  this.cost = 14;
-    if (this.size === medium) {
+Pizza.prototype.getSizeCost = function() {
+    this.cost = 14;
+  if (this.size === "medium") {
     this.cost += 4;
-  } else if (this.size === large)
+  } else if (this.size === "large") {
     this.cost += 8;
+  }
+  if (this.toppings.length <= 2) {
+    this.cost += 2
+  } else if (this.toppings.length <= 4){
+    this.cost += 4
+  } else if (this.toppings.length <= 6) {
+    this.cost += 6
+  }
 };
 
-Pizza.prototype.addUpToppings = function() {
-  this.toppings.forEach(function(){
-    return this.cost += 1;
-  });
-}
+
+
 
 
 
 $(document).ready(function(){
+  // $("#show-pizza-choice").hide();
+
   $("#submit-button").click(function(){
+    $("#show-pizza-choice").show();
     var pizzaSize = $("input[name=size]:checked").val();
 
     var pizzaToppings = [];
     $("input[name=toppings]:checked").each(function(){
       pizzaToppings.push($(this).val());
-    })
-  
+    });
 
     var newPizza = new Pizza(pizzaSize,pizzaToppings)
-    newPizza.size = pizzaSize;
     newPizza.toppings = pizzaToppings;
+    newPizza.getSizeCost();
     console.log(newPizza);
 
 
-    // var pizzaToppings = [];
-    // $("input[name=toppings]:checked").each(function(topping) {
-    //   pizzaToppings += $(this).val() + ","
-    // });
-    // pizzaToppings.split(" ")
-    // console.log(pizzaToppings);
+
+    $("#show-pizza-choice p").text("a " + newPizza.size + " pizza");
 
 
 
 
 
   });
+
+
+
+
+
+
+
 });
